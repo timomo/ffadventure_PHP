f3773796d031cd26466d9cf50ce12615b423e169
Timomo <tyomo88@gmail.com>
2015-01-20 12:25:50 +0900

    ランキング機能の実装開始4

--- lib/ranking.php
+++ lib/ranking.php
@@ -9,11 +9,11 @@ function ranking( $in ) {
     $charas = load_all_chara_data();
     $num_chara = count( $charas );
     $rank_top = read_config_option( "rank_top" );
+    $chara_syoku = read_config_option( "chara_syoku" );
+    $limit = read_config_option( "limit" );
+    $ima = time();
     
-    foreach( $charas as $id => $data ) {
-        var_dump( $data );
-        
-    }
+    $ranking = usort( $ranking, "ranking_compare" );
     
     show_header();
     ?>
@@ -33,5 +33,45 @@ function ranking( $in ) {
             <th>削除まで</th>
         </tr>
     <?php
+    $i = 0;
+    foreach ( $ranking as $id => $data ) {
+        if ( $i > $rank_top ) {
+            break;
+        }
+        $date = $data["date"] + ( 60 * 60 * 24 * $limit );
+        $niti = $date - $ima;
+        $niti = (int)( $niti / ( 60 * 60 * 24 ) );
+        ?>
+        <tr>
+            <td align="center"><?php echo $i ?></td>
+            <td><?php echo $data["name"] ?></td>
+            <td><?php echo $chara_syoku[ $data["syoku"] ] ?></td>
+            <td><a href="<?php echo $data["url"] ?>" target="_blank"><?php echo $data["site"] ?></a></td>
+            <td><?php echo $data["lv"] ?></td>
+            <td><?php echo $data["ex"] ?></td>
+            <td align="center"><?php echo $data["hp"] ?>/<?php echo $data["maxhp"] ?></td>
+            <td align="center"><?php echo $data["n_0"] ?></td>
+            <td align="center"><?php echo $niti ?></td>
+        </tr>
+        <?php
+        $i++;
+    }
+    
     show_footer();
+}
+
+function ranking_compare( $a, $b ) {
+    if ( $b["total"] > $a["total"] ) {
+        return 1;
+    }
+    if ( $b["kati"] > $a["kati"] ) {
+        return 1;
+    }
+    if ( $b["total"] < $a["total"] ) {
+        return -1;
+    }
+    if ( $b["kati"] < $a["kati"] ) {
+        return -1;
+    }
+    return 0;
 }
